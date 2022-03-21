2c5633b8437c183c1a69fb86d9c173163db40beb
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-03-21 12:47:21 +0100

    Added bank account and test

---
+++ src/test/java/DomæneObjekter/KontoTest.java
@@ -0,0 +1,22 @@
+package DomæneObjekter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KontoTest {
+
+
+
+    @Test
+    void indsæt() {
+        Konto konto = new Konto("Chris","123",420);
+        assertEquals(520,konto.indsæt(100));
+    }
+
+    @Test
+    void indsætNegativ() {
+        Konto konto = new Konto("Chris","123",200);
+        assertEquals(200,konto.indsæt(-100));
+    }
+}
